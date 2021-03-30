379eb40eb370aa7d18378c3b76a06fb733bb6a0a
yijiafeng <hypocrisydmc@163.com>
2021-03-30 11:50:49 +0800

    substring

--- slice/slice.go
+++ slice/slice.go
@@ -1,5 +1,7 @@
 package slice
 
+import "fmt"
+
 type AssistSlice struct {
 
 }
@@ -25,4 +27,18 @@ func (a *AssistSlice) Strings2Map(src []string) map[string]struct{} {
 		uniqMap[v] = struct{}{}
 	}
 	return uniqMap
+}
+
+func (a *AssistSlice)GetSubStrByLen(str string, len int) string {
+	var n, i int
+	for i = range str {
+		fmt.Println("i: ", i)
+		if n == len {
+			break
+		}
+		n++
+	}
+	s := str[:i]
+	fmt.Println(s)
+	return s
 }
